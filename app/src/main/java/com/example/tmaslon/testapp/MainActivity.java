7f5f7e2b6a50e7ac0bab51ca96b00f02fb528d73
tomek2k4 <tomasz.maslon@gmail.com>
2016-01-26 16:53:26 +0100

    Added toolbar instead of action bar(more material design)

--- app/src/main/java/com/example/tmaslon/testapp/MainActivity.java
+++ app/src/main/java/com/example/tmaslon/testapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.tmaslon.testapp;
 
-
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+
 import com.example.tmaslon.testapp.fragment.LoginFragment;
 
+import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
 
@@ -13,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         getFragmentManager().beginTransaction().add(R.id.content_main, new LoginFragment()).addToBackStack(null).commit();
     }
