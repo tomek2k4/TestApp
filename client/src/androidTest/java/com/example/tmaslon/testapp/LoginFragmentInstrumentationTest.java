e24fb36cf8ab8ac298bcd0eadf724b0bc55112ba
U-CORP\tmaslon <tmaslon@FRSREDPOL-0001.corp.ad.activcard.com>
2016-05-16 16:24:18 +0200

    Added testing instrumental,added test for login action

--- client/src/androidTest/java/com/example/tmaslon/testapp/LoginFragmentInstrumentationTest.java
+++ client/src/androidTest/java/com/example/tmaslon/testapp/LoginFragmentInstrumentationTest.java
@@ -1,5 +1,7 @@
 package com.example.tmaslon.testapp;
 
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.action.ViewActions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,9 +18,11 @@ import static android.test.ViewAsserts.assertOnScreen;
  * Created by tomasz on 06.03.2016.
  */
 public class LoginFragmentInstrumentationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public static final String TEST_USERNAME = "tomek";
+    private static final String TEST_PASSWORD = "hidstudent";
     private MainActivity mainActivity;
     private EditText usernameEditText;
-    private TextView mHelloWorldTextView;
+    private EditText passwordEditText;
     private Button buttonLogin;
 
 
@@ -33,15 +37,31 @@ public class LoginFragmentInstrumentationTest extends ActivityInstrumentationTes
 
         mainActivity = getActivity();
         usernameEditText = (EditText) mainActivity.findViewById(R.id.username);
+        passwordEditText = (EditText) mainActivity.findViewById(R.id.password);
         buttonLogin = (Button) mainActivity.findViewById(R.id.enter);
     }
 
 
-    public void testUsernameEditText(){
+    public void testUsernameEditTextExists(){
         assertOnScreen(mainActivity.getWindow().getDecorView(),usernameEditText);
     }
 
-    public void testButton() {
+    public void testPaswordEditTextExists(){
+        assertOnScreen(mainActivity.getWindow().getDecorView(),passwordEditText);
+    }
+
+    public void testButtonExistsWithLoginString() {
+        assertOnScreen(mainActivity.getWindow().getDecorView(),buttonLogin);
         onView(withId(R.id.enter)).check(matches(withText("Login")));
     }
+
+    public void testLoginFeature(){
+        ViewActions.typeText(TEST_USERNAME);
+        onView(withId(R.id.username)).perform(ViewActions.typeText(TEST_USERNAME));
+        onView(withId(R.id.password)).perform(ViewActions.typeText(TEST_PASSWORD));
+        onView(withId(R.id.enter)).perform(ViewActions.click());
+
+    }
+
+
 }
